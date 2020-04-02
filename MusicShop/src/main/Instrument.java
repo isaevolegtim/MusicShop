3e435bc1b0fa0362d97198356ed09bfab257fe76
Issayev Oleg <isaevolegtim@gmail.com>
2020-04-02 18:32:45 +0600

    initial commit

---
+++ MusicShop/src/main/Instrument.java
@@ -0,0 +1,34 @@
+import java.math.BigDecimal;
+
+public abstract class Instrument {
+    private BigDecimal price;
+    private String firm;
+    private String material;
+
+    public abstract void play();
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getMFirm() {
+        return firm;
+    }
+
+    public void setFirm(String color) {
+        this.firm = firm;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String Material) {
+        this.material = material;
+    }
+}
+
